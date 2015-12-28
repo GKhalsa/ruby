28c1389df8b2bd3806292bb12894190bb52d59af
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2015-12-27 17:15:38 -0700

    refactor  @num

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -2,27 +2,18 @@ class Squares
   VERSION = 1
 
   def initialize(num)
-    @num = num
-    #binding.pry
-  end
-
-  def numbers
-    numbers = []
-    1.upto(@num) do |i|
-      numbers << i
-    end
-    numbers
+    @num = (0..num)
   end
 
   def square_of_sums
-    total = numbers.reduce(0) do |sum, num|
+    total = @num.reduce(0) do |sum, num|
       sum + num
     end
     total ** 2
   end
 
   def sum_of_squares
-    total = numbers.reduce(0) do |sum, num|
+    total = @num.reduce(0) do |sum, num|
       sum + num ** 2
     end
   end
